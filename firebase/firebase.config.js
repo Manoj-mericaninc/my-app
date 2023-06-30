b180d69fe782b18a96970968b3983230841cbae3
Manoj Kumar <manoj@mericaninc.com>
2023-06-30 12:36:49 +0530

    firebase config removed

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -8,14 +10,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCPuI2KhiqM_VMlvj_n0VUfLSkpRGPrN2M",
-  authDomain: "next-todos-8ad7d.firebaseapp.com",
-  projectId: "next-todos-8ad7d",
-  storageBucket: "next-todos-8ad7d.appspot.com",
-  messagingSenderId: "753765540554",
-  appId: "1:753765540554:web:9ddfcc2fd2c818f8368a33",
-};
+const firebaseConfig = {};
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
